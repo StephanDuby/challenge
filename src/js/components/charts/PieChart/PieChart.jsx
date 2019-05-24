47893fe348b97154a12872dfb0a625099b48424a
Stephan Duby <stephan.duby@etecture.de>
2019-05-24 17:49:09 +0200

    responsive chart container

--- src/js/components/charts/PieChart/PieChart.jsx
+++ src/js/components/charts/PieChart/PieChart.jsx
@@ -1,37 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
-import { lightenColor } from '../../../helpers/colorHelper';
 import {
     aggregateData,
     calulateSegmentPath,
     calculateStartChunkPath,
     calculateEndChunkPath,
-    calculateActiveSegmentPath
+    calculateActiveSegmentPath,
+    calculateButtonPositionOnActiveSegment
 } from './dataHelper';
 import { data } from '../../../mock/pieChartData';
+import { device } from '../../../helpers/viewportSizes';
 
-const CHART_HEIGHT = 300;
-const HEIGHT_UNIT = 'px';
-const CENTER = CHART_HEIGHT / 2;
-const RADIUS = CENTER;
+const CHART_BASE = 320;
+const CHART_PADDING = 30;
+const CHART_HEIGHT = CHART_BASE - 2 * CHART_PADDING;
+const MEASURE_UNIT = 'px';
+const CENTER = CHART_BASE / 2;
+const RADIUS = CHART_HEIGHT / 2;
 const BAR_THICKNESS = 25;
 const BAR_THICKNESS_ACTIVE = 35;
 const BAR_ANGLE_FACTOR = (BAR_THICKNESS / CHART_HEIGHT) * 2;
 
 const ChartContainer = styled.div`
     width: 100%;
-    height: ${CHART_HEIGHT + HEIGHT_UNIT};
+    height: ${CHART_BASE + MEASURE_UNIT};
     display: flex;
+    flex-wrap: wrap;
 `;
 
 const GraphContainer = styled.div`
-    height: ${CHART_HEIGHT + HEIGHT_UNIT};
-    flex: 1;
-    max-width: 50%;
+    height: ${CHART_BASE + MEASURE_UNIT};
+    flex: 1 0;
+    width: 100%;
+    min-width: 320px;
+    max-width: 100%;
+
+    @media ${device.medium} {
+        max-width: 40%;
+    }
 `;
 
 const LegendContainer = styled.div`
     flex: 1;
+    padding: ${CHART_PADDING + MEASURE_UNIT};
 `;
 
 const SVGContainer = styled.svg`
@@ -43,6 +54,14 @@ const PieSegment = styled.path`
 `;
 
 const CircleMask = styled.circle`
+    pointer-events: none;
+`;
+
+const ActiveElementButton = styled.circle`
+    cursor: pointer;
+`;
+
+const ActiveElementButtonPath = styled.path`
     pointer-events: none;
 `;
 
@@ -62,6 +81,12 @@ export class PieChart extends React.Component {
         });
     };
 
+    handleCloseActiveSegment = () => {
+        this.setState({
+            activeSegment: null
+        });
+    };
+
     render() {
         const { activeSegment } = this.state;
         let startAngle = 0;
@@ -73,6 +98,7 @@ export class PieChart extends React.Component {
         };
 
         let activeElement = null;
+        let activeElementButton = null;
 
         pieChartData.aggregatedDefects.forEach((item, index) => {
             const segmentAngle = (item.count * Math.PI * 2) / pieChartData.total;
@@ -82,7 +108,8 @@ export class PieChart extends React.Component {
                 barThickness: BAR_THICKNESS,
                 angleFactor: BAR_ANGLE_FACTOR,
                 startAngle,
-                endAngle: startAngle + segmentAngle
+                endAngle: startAngle + segmentAngle,
+                positioningFactor: CHART_PADDING
             };
 
             svgElements.startChunks.push(calculateStartChunkPath(calculationParameters));
@@ -98,6 +125,11 @@ export class PieChart extends React.Component {
                     path: calculateActiveSegmentPath(calculationParameters),
                     color: item.color
                 };
+
+                activeElementButton = {
+                    position: calculateButtonPositionOnActiveSegment(calculationParameters),
+                    radius: BAR_THICKNESS
+                };
             }
 
             startAngle += segmentAngle;
@@ -106,7 +138,7 @@ export class PieChart extends React.Component {
         return (
             <ChartContainer>
                 <GraphContainer>
-                    <SVGContainer width={'100%'} height={'100%'} viewBox={`0 0 ${CHART_HEIGHT} ${CHART_HEIGHT}`}>
+                    <SVGContainer width={'100%'} height={'100%'} viewBox={`0 0 ${CHART_BASE} ${CHART_BASE}`}>
                         {svgElements.startChunks.map((path, index) => (
                             <PieSegment
                                 key={`start_${index}`}
@@ -141,11 +173,47 @@ export class PieChart extends React.Component {
                             <>
                                 <CircleMask cx={CENTER} cy={CENTER} r={RADIUS} fill={'rgba(255, 255, 255, 0.6)'} />
                                 <PieSegment fill={activeElement.color} d={activeElement.path} />
+                                <ActiveElementButton
+                                    cx={activeElementButton.position.x}
+                                    cy={activeElementButton.position.y}
+                                    r={activeElementButton.radius}
+                                    fill={'rgba(255, 255, 255, 0.8)'}
+                                    onClick={this.handleCloseActiveSegment}
+                                />
+                                <ActiveElementButtonPath
+                                    d={`M${activeElementButton.position.x -
+                                        activeElementButton.radius / 3} ${activeElementButton.position.y -
+                                        activeElementButton.radius / 3} L ${activeElementButton.position.x +
+                                        activeElementButton.radius / 3} ${activeElementButton.position.y +
+                                        activeElementButton.radius / 3}`}
+                                    stroke={'#7D7D7D'}
+                                    onClick={this.handleCloseActiveSegment}
+                                />
+                                <ActiveElementButtonPath
+                                    d={`M${activeElementButton.position.x +
+                                        activeElementButton.radius / 3} ${activeElementButton.position.y -
+                                        activeElementButton.radius / 3} L ${activeElementButton.position.x -
+                                        activeElementButton.radius / 3} ${activeElementButton.position.y +
+                                        activeElementButton.radius / 3}`}
+                                    stroke={'#7D7D7D'}
+                                    onClick={this.handleCloseActiveSegment}
+                                />
                             </>
                         )}
                     </SVGContainer>
                 </GraphContainer>
-                <LegendContainer>here are the legends</LegendContainer>
+                <LegendContainer>
+                    <div>{pieChartData.total} defects</div>
+                    <div>
+                        {pieChartData.aggregatedDefects.map(item => (
+                            <div key={item.defect}>
+                                {item.defect}
+                                {item.count}
+                                {item.color}
+                            </div>
+                        ))}
+                    </div>
+                </LegendContainer>
             </ChartContainer>
         );
     }
--- src/js/components/charts/PieChart/dataHelper.js
+++ src/js/components/charts/PieChart/dataHelper.js
@@ -19,16 +19,28 @@ const getDistinctValues = (data, property) => {
 };
 
 export const calulateSegmentPath = parameters => {
-    const { radiusL, radiusS, startAngle, endAngle } = parameters;
+    const { radiusL, radiusS, startAngle, endAngle, positioningFactor } = parameters;
     const sinAlpha = Math.sin(startAngle);
     const cosAlpha = Math.cos(startAngle);
     const sinBeta = Math.sin(endAngle);
     const cosBeta = Math.cos(endAngle);
 
-    const pointA = { x: radiusL + radiusL * sinAlpha, y: radiusL - radiusL * cosAlpha };
-    const pointB = { x: radiusL + radiusL * sinBeta, y: radiusL - radiusL * cosBeta };
-    const pointC = { x: radiusL + radiusS * sinBeta, y: radiusL - radiusS * cosBeta };
-    const pointD = { x: radiusL + radiusS * sinAlpha, y: radiusL - radiusS * cosAlpha };
+    const pointA = {
+        x: positioningFactor + radiusL + radiusL * sinAlpha,
+        y: positioningFactor + radiusL - radiusL * cosAlpha
+    };
+    const pointB = {
+        x: positioningFactor + radiusL + radiusL * sinBeta,
+        y: positioningFactor + radiusL - radiusL * cosBeta
+    };
+    const pointC = {
+        x: positioningFactor + radiusL + radiusS * sinBeta,
+        y: positioningFactor + radiusL - radiusS * cosBeta
+    };
+    const pointD = {
+        x: positioningFactor + radiusL + radiusS * sinAlpha,
+        y: positioningFactor + radiusL - radiusS * cosAlpha
+    };
 
     const largeArc = endAngle - startAngle > Math.PI;
 
@@ -40,7 +52,7 @@ export const calulateSegmentPath = parameters => {
 };
 
 export const calculateEndChunkPath = parameters => {
-    const { radiusL, radiusS, startAngle, barThickness, angleFactor } = parameters;
+    const { radiusL, radiusS, startAngle, barThickness, angleFactor, positioningFactor } = parameters;
     const chunkStartAngle = startAngle - angleFactor;
     const chunkEndAngle = startAngle;
     const chunkExtraEndAngle = startAngle + 0.1;
@@ -50,10 +62,22 @@ export const calculateEndChunkPath = parameters => {
     const cosBeta = Math.cos(chunkEndAngle);
     const sinGamma = Math.sin(chunkExtraEndAngle);
     const cosGamma = Math.cos(chunkExtraEndAngle);
-    const pointA = { x: radiusL + radiusL * sinAlpha, y: radiusL - radiusL * cosAlpha };
-    const pointB = { x: radiusL + radiusL * sinGamma, y: radiusL - radiusL * cosGamma };
-    const pointC = { x: radiusL + radiusS * sinGamma, y: radiusL - radiusS * cosGamma };
-    const pointD = { x: radiusL + radiusS * sinBeta, y: radiusL - radiusS * cosBeta };
+    const pointA = {
+        x: positioningFactor + radiusL + radiusL * sinAlpha,
+        y: positioningFactor + radiusL - radiusL * cosAlpha
+    };
+    const pointB = {
+        x: positioningFactor + radiusL + radiusL * sinGamma,
+        y: positioningFactor + radiusL - radiusL * cosGamma
+    };
+    const pointC = {
+        x: positioningFactor + radiusL + radiusS * sinGamma,
+        y: positioningFactor + radiusL - radiusS * cosGamma
+    };
+    const pointD = {
+        x: positioningFactor + radiusL + radiusS * sinBeta,
+        y: positioningFactor + radiusL - radiusS * cosBeta
+    };
 
     const largeArc = chunkEndAngle - chunkStartAngle > Math.PI;
 
@@ -66,7 +90,7 @@ export const calculateEndChunkPath = parameters => {
 };
 
 export const calculateStartChunkPath = parameters => {
-    const { radiusL, radiusS, endAngle, barThickness, angleFactor } = parameters;
+    const { radiusL, radiusS, endAngle, barThickness, angleFactor, positioningFactor } = parameters;
     const chunkStartAngle = endAngle + angleFactor;
     const chunkEndAngle = endAngle;
     const chunkExtraEndAngle = endAngle - 0.1;
@@ -76,10 +100,22 @@ export const calculateStartChunkPath = parameters => {
     const cosBeta = Math.cos(chunkEndAngle);
     const sinGamma = Math.sin(chunkExtraEndAngle);
     const cosGamma = Math.cos(chunkExtraEndAngle);
-    const pointA = { x: radiusL + radiusL * sinAlpha, y: radiusL - radiusL * cosAlpha };
-    const pointB = { x: radiusL + radiusL * sinGamma, y: radiusL - radiusL * cosGamma };
-    const pointC = { x: radiusL + radiusS * sinGamma, y: radiusL - radiusS * cosGamma };
-    const pointD = { x: radiusL + radiusS * sinBeta, y: radiusL - radiusS * cosBeta };
+    const pointA = {
+        x: positioningFactor + radiusL + radiusL * sinAlpha,
+        y: positioningFactor + radiusL - radiusL * cosAlpha
+    };
+    const pointB = {
+        x: positioningFactor + radiusL + radiusL * sinGamma,
+        y: positioningFactor + radiusL - radiusL * cosGamma
+    };
+    const pointC = {
+        x: positioningFactor + radiusL + radiusS * sinGamma,
+        y: positioningFactor + radiusL - radiusS * cosGamma
+    };
+    const pointD = {
+        x: positioningFactor + radiusL + radiusS * sinBeta,
+        y: positioningFactor + radiusL - radiusS * cosBeta
+    };
 
     const largeArc = chunkEndAngle - chunkStartAngle > Math.PI;
 
@@ -92,7 +128,7 @@ export const calculateStartChunkPath = parameters => {
 };
 
 export const calculateActiveSegmentPath = parameters => {
-    const { radiusL, radiusS, startAngle, endAngle, angleFactor, barThickness } = parameters;
+    const { radiusL, radiusS, startAngle, endAngle, angleFactor, barThickness, positioningFactor } = parameters;
     const outerStartAngle = startAngle - angleFactor;
     const innerEndAngle = endAngle - angleFactor;
     const sinAlpha = Math.sin(outerStartAngle);
@@ -104,10 +140,22 @@ export const calculateActiveSegmentPath = parameters => {
     const sinDelta = Math.sin(innerEndAngle);
     const cosDelta = Math.cos(innerEndAngle);
 
-    const pointA = { x: radiusL + radiusL * sinAlpha, y: radiusL - radiusL * cosAlpha };
-    const pointB = { x: radiusL + radiusL * sinBeta, y: radiusL - radiusL * cosBeta };
-    const pointC = { x: radiusL + radiusS * sinDelta, y: radiusL - radiusS * cosDelta };
-    const pointD = { x: radiusL + radiusS * sinGamma, y: radiusL - radiusS * cosGamma };
+    const pointA = {
+        x: positioningFactor + radiusL + radiusL * sinAlpha,
+        y: positioningFactor + radiusL - radiusL * cosAlpha
+    };
+    const pointB = {
+        x: positioningFactor + radiusL + radiusL * sinBeta,
+        y: positioningFactor + radiusL - radiusL * cosBeta
+    };
+    const pointC = {
+        x: positioningFactor + radiusL + radiusS * sinDelta,
+        y: positioningFactor + radiusL - radiusS * cosDelta
+    };
+    const pointD = {
+        x: positioningFactor + radiusL + radiusS * sinGamma,
+        y: positioningFactor + radiusL - radiusS * cosGamma
+    };
 
     const largeArc = endAngle - startAngle > Math.PI;
 
@@ -120,4 +168,17 @@ export const calculateActiveSegmentPath = parameters => {
     } A ${barThickness} ${barThickness} 0 ${largeArc ? '1,1' : '0,1'} ${pointA.x} ${pointA.y}`;
 
     return path;
+};
+
+export const calculateButtonPositionOnActiveSegment = parameters => {
+    const { radiusL, startAngle, endAngle, positioningFactor, angleFactor } = parameters;
+    const actualStartAngle = startAngle - angleFactor;
+    const positionAngle = actualStartAngle + (endAngle - actualStartAngle) / 2;
+    const sinAlpha = Math.sin(positionAngle);
+    const cosAlpha = Math.cos(positionAngle);
+    const pointA = {
+        x: positioningFactor + radiusL + radiusL * sinAlpha,
+        y: positioningFactor + radiusL - radiusL * cosAlpha
+    };
+    return pointA;
 };
